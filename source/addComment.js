fb870f0c2933168bf0615da0c3766cc4c8900315
Edilene <edilene_cruz@yahoo.com.br>
2017-05-03 00:19:42 -0600

    completes comments

--- source/addComment.js
+++ source/addComment.js
@@ -1,3 +1,21 @@
 $(document).ready(function(){
-  // add your code here
+  $('#new_comment_button').on('click', function() {
+    $('form').toggle()
+  })
+
+  $('form[name=new_comment]').on('submit', function(event) {
+    event.preventDefault();
+    var $li = $('<li />');
+    var $span = $('<span />').addClass('author');
+    var $comment = $(this).find('textarea[name=comment]');
+    var $author = $(this).find('input[name=authorName]');
+
+    $li.text($comment.val());
+    $span.text($author.val());
+
+    $li.append($span);
+
+    var $list = $('#comment_list');
+      $list.append($li);
+  })
 })
