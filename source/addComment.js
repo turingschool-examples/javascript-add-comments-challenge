4c1dd6a324d2235c799b810b93159101179e7720
caward12 <ward.colleen.a@gmail.com>
2017-06-27 16:25:15 -0600

    adds comment funtionality

--- source/addComment.js
+++ source/addComment.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
-  // add your code here
+    $('#new_comment_button').click(function(){
+      event.preventDefault();
+      $('#new_comment_button').hide();
+      $('#comments').append('<div id="form"><textarea rows="5" cols="70" name="commenttext" placeholder="comment text here"/><br><input type="text" name="poster" placeholder="author"/><input type="submit" value="create comment"</div>')
+        $('#comments input[type="submit"]').click(function(){
+          event.preventDefault();
+          var commenttext = $(this).parent().find('textarea').val()
+          var author = $(this).parent().find('input[name="poster"]').val()
+          if (commenttext) {
+            $('#comment_list').append('<li>' + commenttext + '<span class="author">' + author + '</span></li>')
+            $('#form').remove();
+            $('#new_comment_button').show();
+          }
+          else{
+            alert("You must enter comment text!")
+          }
+        })
+    })
 })
