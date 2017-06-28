35d08af9babb872bfd83b98e1401e9ca875fc628
VictoriaVasys <vvasys@gmail.com>
2017-06-28 07:05:10 -0600

    Add comments, hide/show new comment button

--- source/addComment.js
+++ source/addComment.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
-  // add your code here
+  $('#new_comment_button').click(function() {
+    event.preventDefault()
+    $(this).hide()
+    $('#comments').append('<div id="form"><textarea rows="3" cols="60" name="text" placeholder="Your comment here..." /><br><input type ="text" id="author" placeholder="author" /><input type="submit" value="Submit Comment" /></div>')
+    $('#comments input[type="submit"]').click(function() {
+      event.preventDefault()
+      var text = $(this).parent().find('textarea').val()
+      var author = $(this).parent().find('input[id="author"]').val()
+      if (text) {
+        $('#comment_list').append('<li>' + text + '<span class="author">' + author + '</span></li>')
+        $('#form').remove()
+        $('#new_comment_button').show()
+      } else {
+        alert("Your comment must have text to submit.")
+      }
+    })
+  })
 })
