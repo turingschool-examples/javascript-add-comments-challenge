fe2fa07d5538ecdf1f544454ff4c857cad2e4873
Lauren Oliveri <lauren.oliveri.9@gmail.com>
2017-06-28 09:33:22 -0600

    finish new comments feature in jQuery

--- source/addComment.js
+++ source/addComment.js
@@ -1,3 +1,27 @@
 $(document).ready(function(){
-  // add your code here
+
+  function addCommentBox() {
+    $('#comments').append('<form id="new_comment"></form>')
+    $('#new_comment').append('<textarea placeholder="Your comment here..."></textarea>')
+    $('#new_comment').append('<input id="authorName" placeholder="author" />')
+    $('#new_comment').append('<input type="submit" value="Submit Comment" />')
+  }
+
+  $('#new_comment_button').on('click', function(){
+    event.preventDefault();
+    $(this).hide();
+    addCommentBox();
+
+    $('#new_comment').submit(function(event){
+      event.preventDefault();
+      var comment_body = $('textarea').val()
+      if (comment_body.length) {
+        var author_name = '\n<span class="author">' + $('#authorName').val() + '</span>'
+        $('#comment_list').append('<li>' + comment_body + author_name + '</li>')
+        $('#new_comment_button').show();
+        $('#new_comment').remove();
+      }
+    })
+  })
+
 })
