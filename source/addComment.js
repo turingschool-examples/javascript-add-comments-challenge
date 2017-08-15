6d79021479b351822bfae640624c7d5420d49eed
ccorrigan <charlie.ps.corrigan@gmail.com>
2017-08-15 17:16:58 -0600

    Add comment functionality

--- source/addComment.js
+++ source/addComment.js
@@ -1,3 +1,21 @@
 $(document).ready(function(){
-  // add your code here
+  $('#new_comment_button').on('click', function() {
+    $('#new_comment').toggle(
+      function(){$('#new_comment').css({display: 'block'})},
+      function(){$('#new_comment_button').css({display: 'none'})}
+    );
+  });
+
+  $('#new_comment').on('submit', function() {
+    var comment = $('textarea[name="comment"]').val();
+    var author = $('input[name="author"]').val();
+    event.preventDefault();
+    $('#comment_list').append('<li>' + comment + '<span class="author">' + author + '</span></li>')
+    $('#new_comment').toggle(
+      function(){$('#new_comment').css({display: 'none'})},
+      function(){$('#new_comment_button').css({display: 'block'})}
+    );
+    $('textarea[name="comment"]').val("");
+    $('input[name="author"]').val("");
+  });
 })
