85cf3b2e8b74f57d966bcc483ad29fd254e477b7
Aaron Olsen <mraaronolsen@gmail.com>
2017-08-15 12:53:21 -0600

    Comments can be created

--- source/addComment.js
+++ source/addComment.js
@@ -1,3 +1,33 @@
 $(document).ready(function(){
-  // add your code here
+
+  function commentForm(){
+    return `<form id='new_comment'>
+      Author: <input id='authorName'><br>
+      Comment: <textarea id='text'></textarea>
+      <input type='submit'>
+    </form>`
+  };
+
+  function newComment(text, author){
+    return `<li>
+      ${text}
+      <span class="author">${author}</span>
+    </li>`
+  };
+
+  $('#new_comment_button').on('click', function(){
+    let authorName;
+    let text;
+    let form = $('#comment_list').append(commentForm());
+
+    $('#new_comment').on('submit', function(){
+      event.preventDefault();
+
+      authorName = $('#authorName').val();
+      text = $('#text').val();
+
+      $('#new_comment').remove()
+      $('#comment_list').append(newComment(text, authorName));
+    });
+  });
 })
