64bda17409d7e2090f62847772402d65e46543e9
squeemishly <squeemishly@gmail.com>
2017-08-16 08:47:50 -0600

    moved logic to seperate functions

--- source/addComment.js
+++ source/addComment.js
@@ -6,26 +6,23 @@ const commentForm = `
 </form>
 `
 
-// const showForm = function() {
-//   $("#new_comment_button").before(commentForm)
-//   $("#new_comment_button").hide()
-//   event.preventDefault();
-// }
+const showForm = function() {
+  $("#new_comment_button").before(commentForm)
+  $("#new_comment_button").hide()
+  event.preventDefault();
+  $("#comment_form").on("submit", submitComment)
+}
+
+const submitComment = function(event) {
+  let comment = $("[name=comment_field]").val();
+  let author = $("[name=comment_author]").val();
+
+  $("#comment_list").append("<li>" + comment + "<span class='author'>" + author + "</span></li>")
+  $("#comment_form").remove()
+  $("#new_comment_button").show();
+  event.preventDefault();
+}
 
 $(document).ready(function(){
-  // $("#new_comment_button").on("click", showForm())
-  $("#new_comment_button").on("click", function(event) {
-    $("#new_comment_button").before(commentForm)
-    $("#new_comment_button").hide()
-    event.preventDefault();
-    $("#comment_form").on("submit", function(event) {
-      let comment = $("[name=comment_field]").val();
-      let author = $("[name=comment_author]").val();
-
-      $("#comment_list").append("<li>" + comment + "<span class='author'>" + author + "</span></li>")
-      $("#comment_form").remove()
-      $("#new_comment_button").show();
-      event.preventDefault();
-    })
-  })
+  $("#new_comment_button").on("click", showForm)
 })
