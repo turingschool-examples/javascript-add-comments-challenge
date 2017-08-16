ed3c988df6bf90d6eed0307f2bc4df911f58bf1e
rwilhel <rwilhel@gmail.com>
2017-08-15 21:17:40 -0600

    Creates variable assignments for form field input

--- source/addComment.js
+++ source/addComment.js
@@ -8,6 +8,12 @@ $(document).ready(function(){
     $('body').append('<form id="new_comment" name="new_comment" style="display: block"></form>');
     $('#new_comment').append('<textarea form="new_comment" id="comment-text" placeholder="Your comment here!"/>');
     $('#new_comment').append('<input type="text" id="authorName" placeholder="Your name here!"/>');
-    $("#new_comment").append('<input type="submit id="createComment" value="Leave Comment!"/>');
+    $("#new_comment").append('<input type="submit" id="createComment" value="Leave Comment!"/>');
+
+      $('#createComment').on('click', function () {
+        event.preventDefault();
+        var comment = $('#comment-text').val();
+        var author = $('#authorName').val();
+      })
   });
 });
