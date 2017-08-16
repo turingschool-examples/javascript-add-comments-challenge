a0ab07c7543b088e5197a44e522acc4a893bb0e4
William Thomas <wthoma390@gmail.com>
2017-08-15 19:35:53 -0600

    complete comment challenge

--- source/addComment.js
+++ source/addComment.js
@@ -1,3 +1,23 @@
 $(document).ready(function(){
-  // add your code here
+  $('#new_comment_button').on('click', function () {
+    event.preventDefault()
+
+    $('#new_comment_button').hide()
+    $('<form id="new_comment" name="new_comment" style="display: block"></form>').appendTo("body")
+    $('<textarea form="new_comment" id="comment-text" placeholder="Comment here"/>').appendTo("#new_comment")
+    $('<input type="text" id="authorName" placeholder="your name"/>').appendTo("#new_comment")
+    $('<input type="submit" id="createComment" value="Create Comment"/>').appendTo("#new_comment")
+
+    $('#createComment').on('click', function () {
+      event.preventDefault()
+      let comment = $('#comment-text').val();
+      let author  = $('#authorName').val();
+
+      $('#new_comment').remove()
+      if (comment){
+        $('<li>'+comment+'<span class="author">'+author+'</span>' + '</li>').appendTo("#comment_list");
+      }
+      $('#new_comment_button').show();
+    })
+  })
 })
