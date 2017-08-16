9ef8cd79171d7c0cb0acd0eafddb5e4682a7a0ab
rwilhel <rwilhel@gmail.com>
2017-08-15 21:13:31 -0600

    Adds commenting form to js file

--- source/addComment.js
+++ source/addComment.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
-  // add your code here
-})
+
+  $('#new_comment_button').on('click', function() {
+    event.preventDefault();
+
+    $(this).hide()
+
+    $('body').append('<form id="new_comment" name="new_comment" style="display: block"></form>');
+    $('#new_comment').append('<textarea form="new_comment" id="comment-text" placeholder="Your comment here!"/>');
+    $('#new_comment').append('<input type="text" id="authorName" placeholder="Your name here!"/>');
+    $("#new_comment").append('<input type="submit id="createComment" value="Leave Comment!"/>');
+  });
+});
