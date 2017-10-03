3631531e7d67023e57496cb7b76af280e41b186c
BFunk <bret.e.funk@gmail.com>
2017-10-03 16:44:29 -0600

    setting up the basics

--- source/addComment.js
+++ source/addComment.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
-  // add your code here
-})
+    $("#comment_form").hide();
+  $(#comments).on("submit", function() {
+    $("#comment_form").show();
+    jQuery(html [, ownerDocument]);
+    let comment = e.data.val();
+  });
+});
