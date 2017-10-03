588f28a6a734d11741d1b231c6c528dba99058e3
Ellen Cooper <ellencooper74@gmail.com>
2017-10-03 16:06:40 -0600

    completes comment functionality

--- source/addComment.js
+++ source/addComment.js
@@ -1,3 +1,21 @@
 $(document).ready(function(){
-  // add your code here
+  var commentForm = $(".comment-form");
+  console.log(commentForm)
+
+  commentForm.hide();
+
+  $("#new_comment_button").on("click", function(e){
+    commentForm.show()
+  })
+
+  $("#new-comment").on("submit", function(e){
+    commentBody = $("#comment-body").val()
+    author = $("#author-name").val()
+    $('#comment_list').append('<li>' + commentBody + "<span class='author'>" + author + "</span></li>");
+    commentForm.hide();
+    e.preventDefault();
+    $("#comment-body").val('');
+    $("#author-name").val(''); 
+
+  })
 })
