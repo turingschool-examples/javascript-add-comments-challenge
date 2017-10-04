230528db3c29ec02b68bb53cf6806363390adfc1
BFunk <bret.e.funk@gmail.com>
2017-10-03 20:42:01 -0600

    almost working, buts with the newline

--- source/addComment.js
+++ source/addComment.js
@@ -10,7 +10,6 @@ $(document).ready(function(){
       e.preventDefault();
       let $comment = $('#comment').val();
       let $name = $('#user_name').val();
-      $("#comment_list").append(`<li>${$comment}</li>\n<em>${$name}</em></li>`);
-      //debugger;
+      $("#comment_list").append(`<li>${$comment}\n<em><font size="-1">${$name}</font></em></li>`);
     });
 });
