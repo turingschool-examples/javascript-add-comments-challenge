56c4ca6dc92feceb2a16b1b559b5c39877086e68
jtruong2 <jtruong2@comcast.net>
2017-10-04 08:56:54 -0600

    starts add comments challenge

--- source/addComment.js
+++ source/addComment.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
-  // add your code here
-})
+  $('form').hide();
+  $('#comment_list').append($('<input />', {}))
+  $('#new_comment_button').on('click', function(){
+    // $('#new_comment_button').hide();
+    // $('form').show();
+  });
+  $('form').submit(function(event) {
+    event.preventDefault();
+    var comment = $('input[name=comment]').val();
+    var author = $('input[name=author]').val();
+  })
+});
