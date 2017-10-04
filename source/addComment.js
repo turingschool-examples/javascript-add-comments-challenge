5b5dc58bc6eed16f5448388ce540a4345c7c9abc
Chantal Justamond <cjustamm66@me.com>
2017-10-03 18:20:52 -0600

    comments are appending and toggle works after submitting :hatched_chick:

--- source/addComment.js
+++ source/addComment.js
@@ -1,3 +1,32 @@
 $(document).ready(function(){
-  // add your code here
-})
+    $('#form').hide();
+
+    $('#new_comment_button').on('click',() => {
+        $('#form').toggle();
+    });
+
+    function formcheck(comment, author) {
+        if (comment = '')
+            return false;
+        else
+            return true
+    }
+
+    function refresh() {
+        $('#form').toggle(!formcheck)
+    }
+
+    $('#form').submit( (event) =>  {
+
+        let comment = $('#form :input')[0].value;
+        let author = $('#form :input')[1].value;
+        event.preventDefault();
+
+        if (formcheck(comment, author) == true) {
+            $('#comment_list').append('<li>' + comment + '<span class="author">' + author + '</span>' + '</li>');
+            $('#form').hide();
+            $('#form').trigger('reset');
+            refresh();
+        }
+    })
+});
