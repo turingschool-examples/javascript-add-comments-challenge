492139955b287cd65d236cfe8b86ec4fbc1fd800
BFunk <bret.e.funk@gmail.com>
2017-10-03 19:45:45 -0600

    getting text to append but still cant catch the data from submit

--- source/addComment.js
+++ source/addComment.js
@@ -1,26 +1,17 @@
 $(document).ready(function(){
     $("#comment_form").hide();
     $("").hide();
-    //$("textarea").hide();
-    //$("#submit_button").hide();
 
   $("button").on("click", function() {
     $("#comment_form").show();
-    //$("textarea").show();
-    //$("#submit_button").show();
   });
 
     $("#comment_form").on('submit', function(e) {
       e.preventDefault();
       console.log('it works!');
-      let data = $("#comment_form :input");
-      //let data = $("#textarea :input");
-      //let data1 = $("#text_box :input");
-      //let data2 = $("#submit_button :input");
-      console.log(data);
-      //console.log(data1);
-      //console.log(data2);
-      //return $data;
+      let $data = $("#comment_form :input");
+      console.log($data);
+      $("#comment_list").append(`${$data}`);
       debugger;
     });
 });
