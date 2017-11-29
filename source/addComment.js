01b95eb5468334b05432ec385973ca3d54c2b3c6
erose357 <erose357@gmail.com>
2017-11-28 19:45:26 -0700

    Adds functionality to show new comment on the page

--- source/addComment.js
+++ source/addComment.js
@@ -1,7 +1,14 @@
 const commentForm = "<div class='container'><form id='comment-form' name'?'><textarea name='comment' placeholder='My new comment!' style='width:300px; height:50px'></textarea></br><input name='author' placeholder='Author' /><input type='submit' value='Submit Comment' /></form></div>";
 
+const commentPost = "<li>Comment Body<span class='author'>Author Name</span</li>"
+
 $(document).ready(function(){
   $('#new_comment_button').on('click', function() {
     $(commentForm).appendTo('body');
+  });
+
+  $('body').on('click', '#comment-form', function() {
+    $(commentPost).appendTo('ul');
+    event.preventDefault();
   });
 })
