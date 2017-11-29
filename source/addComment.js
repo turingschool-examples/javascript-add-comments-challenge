b1162719329cd1c46cd56f95014f10c4a8f33e1b
samuelssnider <sam_redins@yahoo.com>
2017-11-29 09:02:19 -0700

    mostly working, can't enter my new form

--- source/addComment.js
+++ source/addComment.js
@@ -2,13 +2,21 @@ $(document).ready(function(){
   var createCommentPanel = function() {
     $('#comment_list').append('<form id="temp-comment"><input name="temp-com" placeholder = "Your comment here"/>')
     $('#comment_list').append('<form id="temp-name"><input name="temp-n" placeholder = "Your name here"/>')
-    $('#comment_list').append('<input type="submit" name="add-comment"value="Add your comment!"></form>')
+    $('#comment_list').append('<input type="submit" id="temp-submit" name="add-comment"value="Add your comment!"></form>')
+      document.getElementById("myBtn").addEventListener("click", function(){
+        var newCommentName = document.forms[0].elements[0]
+        var newCommentName = document.forms[0].elements[1]
+        debugger
+        $('#comment_list').remove("#temp-comment")
+        $('#comment_list').remove("#temp-name")
+        $('#comment_list').remove("#temp-submit")
+      });
+    });
   }
   
   var createNewComment = function() {
     
-  }
   
   $("button[name='new_comment_button']").on('click', createCommentPanel)
-  $("button[name='add-comment']").on('click', createNewComment)
+  $("#temp-submit").on('click', createNewComment)
 })
