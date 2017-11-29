e396664eefef05c7061e232261360afed50038f8
Brandon Stewart <boveus@gmail.com>
2017-11-28 18:19:31 -0700

    it works now

--- source/addComment.js
+++ source/addComment.js
@@ -7,17 +7,19 @@ $(document).ready(function(){
     $('#new_comment')
      .append('<input type="text" id="authorName" value="Author Name"> <br>')
      .append('<textarea name="post_body" value="Post Body">')
-     .append('<input type="submit" id="submit_comment" name="submit_comment">')
+     .append('<input type="submit" id="submit_comment">')
   });
 
-  $("#submit_comment").on( "click", function() {
-    $("#comment_list").append('<li></li>');
+  $("body").on("click", "#submit_comment", function(event) {
+    var commentBody = $(new_comment[name="post_body"]).val();
+    var author =  $(new_comment[id="authorName"]).val();
+    var authorSpan = "<span class='author'>" + author + "</span>"
+    $("#comment_list").append('<li>' + commentBody  + authorSpan + '</li>');
+    event.preventDefault();
   });
 });
 
-// var commentBody = $(new_comment[name="post_body"]).val();
-// var author =  $(new_comment[id="authorName"]).val();
-// var authorSpan = "<span class='author'>" + author + "</span>"
+
 
 // + commentBody  + authorSpan +
 // $( "#new_comment" ).remove();
