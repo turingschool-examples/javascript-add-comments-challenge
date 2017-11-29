fe8eb03598dc55e1e4ff489f645c2f37a503da9d
Brandon Stewart <boveus@gmail.com>
2017-11-28 18:15:31 -0700

    add current

--- source/addComment.js
+++ source/addComment.js
@@ -1,3 +1,24 @@
 $(document).ready(function(){
-  // add your code here
-})
+  $( "#new_comment_button" ).on( "click", function() {
+    $("#new_comment_button").hide();
+    var commentForm = "<form id='new_comment'></form>";
+    $("#comments").append(commentForm)
+    $("#new_comment").show();
+    $('#new_comment')
+     .append('<input type="text" id="authorName" value="Author Name"> <br>')
+     .append('<textarea name="post_body" value="Post Body">')
+     .append('<input type="submit" id="submit_comment" name="submit_comment">')
+  });
+
+  $("#submit_comment").on( "click", function() {
+    $("#comment_list").append('<li></li>');
+  });
+});
+
+// var commentBody = $(new_comment[name="post_body"]).val();
+// var author =  $(new_comment[id="authorName"]).val();
+// var authorSpan = "<span class='author'>" + author + "</span>"
+
+// + commentBody  + authorSpan +
+// $( "#new_comment" ).remove();
+// $("#new_comment_button").show();
