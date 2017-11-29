4d4b5d6dceae71fb9502eea96f61572579eab502
erose357 <erose357@gmail.com>
2017-11-28 18:56:10 -0700

    Adds form when 'new comment' button is clicked

--- source/addComment.js
+++ source/addComment.js
@@ -1,3 +1,7 @@
+const commentForm = "<div class='container'><form id='comment-form' name'?'><textarea name='comment' placeholder='My new comment!' style='width:300px; height:50px'></textarea></br><input name='author' placeholder='Author' /><input type='submit' value='Submit Comment' /></form></div>";
+
 $(document).ready(function(){
-  // add your code here
+  $('#new_comment_button').on('click', function() {
+    $(commentForm).appendTo('body');
+  });
 })
