bf16574111e6fb736bb28f96a063f4cfb64f9bd4
erose357 <erose357@gmail.com>
2017-11-28 20:15:59 -0700

    Adds full functionality

--- source/addComment.js
+++ source/addComment.js
@@ -1,14 +1,18 @@
-const commentForm = "<div class='container'><form id='comment-form' name'?'><textarea name='comment' placeholder='My new comment!' style='width:300px; height:50px'></textarea></br><input name='author' placeholder='Author' /><input type='submit' value='Submit Comment' /></form></div>";
-
-const commentPost = "<li>Comment Body<span class='author'>Author Name</span</li>"
+const commentForm = "<div class='container'><form id='comment-form' name'?'><textarea name='comment' placeholder='My new comment!' style='width:300px; height:50px'></textarea></br><input name='author' placeholder='Author' /><input type='submit' value='Create Comment' /></form></div>";
 
 $(document).ready(function(){
   $('#new_comment_button').on('click', function() {
-    $(commentForm).appendTo('body');
+    $(commentForm).appendTo('#comments');
   });
 
-  $('body').on('click', '#comment-form', function() {
-    $(commentPost).appendTo('ul');
+  $('#comments').on('submit', '#comment-form', function() {
+    let comment = $('textarea[name=comment]').val();
+    let author = $('input[name=author]').val();
+    if (comment.length === 0) {
+    } else {
+      $(`<li>${comment}<span class='author'>${author}</span</li>`).appendTo('ul');
+    }
     event.preventDefault();
+    $('#comment-form').remove();
   });
 })
