10ea24b0bf3987aaa6bece97e2c324e3652dcb3d
samuelssnider <sam_redins@yahoo.com>
2017-11-29 08:01:18 -0700

    comment box appearing in the dom, now just need to strip it and add it's fields

--- source/addComment.js
+++ source/addComment.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
-  // add your code here
+  var createCommentPanel = function() {
+    $('#comment_list').append('<form id="temp-comment"><input name="temp-com" placeholder = "Your comment here"/>')
+    $('#comment_list').append('<form id="temp-name"><input name="temp-n" placeholder = "Your name here"/>')
+    $('#comment_list').append('<input type="submit" name="add-comment"value="Add your comment!"></form>')
+  }
+  
+  var createNewComment = function() {
+    
+  }
+  
+  $("button[name='new_comment_button']").on('click', createCommentPanel)
+  $("button[name='add-comment']").on('click', createNewComment)
 })
