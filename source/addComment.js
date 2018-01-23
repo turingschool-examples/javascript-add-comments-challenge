5b9f9cf4b99f5606c27f6cc70a304cd4549e9fa1
Adrian Lara <adrianblara@gmail.com>
2018-01-23 15:49:16 -0700

    new comment button is replaced with new comment form . display:none? why?

--- source/addComment.js
+++ source/addComment.js
@@ -1,3 +1,22 @@
 $(document).ready(function(){
-  // add your code here
+  // on click of New Comment replace button with form
+  $("#new_comment_button").on("click", function(event) {
+    event.preventDefault()
+    replaceButton()
+  })
+  // on click of Submit Comment (on form), replace form with
+  // form content and new button
 })
+
+function replaceButton() {
+  $("#new_comment_button").remove()
+
+  $("#comments").append(
+    "<form id=new_comment>" +
+      "<p><input type='textarea'></p>" +
+      "<input type='text' value='author' id='authorName'>" +
+      "<input type='submit' value='Submit Comment'>" +
+    "</form>"
+  )
+  $("#new_comment").show()
+}
