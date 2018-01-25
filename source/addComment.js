938e8caf9b416eca27ed50f0a938579f0fd90694
Emily Wise <wise.m.emily@gmail.com>
2018-01-24 17:43:07 -0700

    WIP author

--- source/addComment.js
+++ source/addComment.js
@@ -1,3 +1,18 @@
-$(document).ready(function(){
-  // add your code here
-})
+
+  $('button').click(function(event){
+    event.preventDefault();
+    $('#new_comment_button').hide();
+    $('#comments').append(
+      `<input type="text" name="comment" id='comment_form' value="">
+        <input type="text" name="author" id='author_form' value="">
+        <input type="submit" id='form_button' value="button">` );
+  })
+
+  $('#comments').on('click', '#form_button', addComment);
+
+  function addComment(event) {
+    var newComment = $('#comment_form').val();
+    var newAuthor = $('#author_form').val();
+    $('#comment_list').append('<li>' + newComment + '<span>' + newAuthor + '</span>'+ '</li>');
+
+}
