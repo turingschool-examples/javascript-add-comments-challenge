2b044ff624f8380049433a05219641af35782398
leepuppychow <leepuppychow@gmail.com>
2018-03-10 10:58:07 -0700

    Basic functionality working, would be nice to be able to delete comment

--- source/addComment.js
+++ source/addComment.js
@@ -1,3 +1,30 @@
 $(document).ready(function(){
-  // add your code here
+  showOrHideCommentForm();
+  createNewComment();
 })
+
+function showOrHideCommentForm() {
+  $('#new-comment-button').click( () => {
+    $('#new-comment').toggle()
+  })
+}
+
+function createNewComment() {
+  $('#create-comment-button').click( () => {
+    event.preventDefault();
+    let text = $('#comment-text').val();
+    let author = $('#author-name').val();
+    let newComment = `
+      <li>
+        ${text}
+        <span class="author">~~${author}</span>
+      </li>`
+    $(newComment).appendTo('#comment-list')
+    resetCommentFields();
+  })
+}
+
+function resetCommentFields() {
+  $('#comment-text').val("")
+  $('#author-name').val("")
+}
